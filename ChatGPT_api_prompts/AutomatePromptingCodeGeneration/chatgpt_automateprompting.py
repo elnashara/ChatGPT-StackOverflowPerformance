e6df916a695739a9a6c23b1f739c9a6aac7d61c7
elnashara <elnashara@ymail.com>
2023-04-23 22:13:22 -0700

    Add files via upload

---
+++ ChatGPT_api_prompts/AutomatePromptingCodeGeneration/chatgpt_automateprompting.py
@@ -0,0 +1,101 @@
+# Import the generate_code function from ai_coder
+from ai_coder import generate_code
+import os
+import pathlib
+
+dir = os.path.dirname(__file__)
+
+# Create a file named 'api_key' and provide your private API key from the link provided: (https://platform.openai.com/account/api-keys)
+f = open(os.path.join(dir,"api_key"), "r")
+api_key = f.read()
+
+# Define the human messages and remove spaces from the first message
+human_messages_list = [
+    [["Quickest way to find missing number in an array of numbers The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["Quickest way to find missing number in an array of numbers The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["Find the Duplicate Number The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["Find the Duplicate Number The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["Python algorithm to find the indexes of the k smallest number in an unsorted array? The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers and k smallest number as a parameters."],
+     ["Python algorithm to find the indexes of the k smallest number in an unsorted array? The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers and k smallest number as a parameters."]]
+    ,
+    [["Count pairs of elements in an array whose sum equals a given sum (but) do it in a single iteration(!) The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers and target_sum as a parameters."],
+     ["Count pairs of elements in an array whose sum equals a given sum (but) do it in a single iteration(!) The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers and target_sum as a parameters."]]
+    ,
+    [["How do I find the duplicates in a list and create another list with them? The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["How do I find the duplicates in a list and create another list with them? The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["Removing duplicates in lists The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["Removing duplicates in lists The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["Quicksort with Python The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["Quicksort with Python The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["How do I reverse a list or loop over it backwards? The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["How do I reverse a list or loop over it backwards? The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["How to count the frequency of the elements in an unordered list?. The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["How to count the frequency of the elements in an unordered list?. The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["Maximum Product Subarray The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single list of integers as a parameter."],
+     ["Maximum Product Subarray The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single list of integers as a parameter."]]
+    ,
+    [["How to find middle element in a python linked list in a single traversal? The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single head as a parameter."],
+     ["How to find middle element in a python linked list in a single traversal? The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single head as a parameter."]]
+    ,
+    [["How to detect a loop in a linked list? The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single head as a parameter."],
+     ["How to detect a loop in a linked list? The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single head as a parameter."]]
+    ,
+    [["Reversing a linked list in python The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single head as a parameter."],
+     ["Reversing a linked list in python The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single head as a parameter."]]
+    ,
+    [["Finding the length of a linked list in python The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single head as a parameter."],
+     ["Finding the length of a linked list in python The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single head as a parameter."]]
+    ,
+    [["Pascal's Triangle for Python The implementation should be fast. The implementation should be in a function named funcImp1 that takes a single number of raws as a parameter."],
+     ["Pascal's Triangle for Python The implementation should be fast as the size of the list grows. The implementation should be in a function named funcImp2 that takes a single number of raws as a parameter.."]]]
+
+new_dir_name = 'data'
+dir = pathlib.Path(dir, new_dir_name)
+# os.mkdir(dir, exist_ok=True)
+os.makedirs(dir, exist_ok=True)
+
+file_index = 0
+for human_messages in human_messages_list:
+
+    generated_code_0_list =[]
+    generated_code_1_list =[]
+
+    msg_0 = human_messages[0][0].replace('?','').replace('.','')
+    msg_1 = human_messages[1][0].replace('?','').replace('.','')
+    for i in range(10):
+
+        print(i)
+        # Call the generate_code function with prior_code=None
+        generated_code_0 = generate_code([], human_messages[0], api_key, checkfn = compile, max_tries=10, temperature=0)
+        generated_code_0_list.append(generated_code_0)
+
+
+        # Call the generate_code function with prior_code=None
+        generated_code_1 = generate_code([], human_messages[1], api_key, checkfn = compile, max_tries=10, temperature=0)
+        generated_code_1_list.append(generated_code_1)
+        
+
+    
+    filename_0 = os.path.join(dir, "p" + str(file_index) + ".0_" + ""  .join(msg_0[0:100].split()) + ".py")
+    # Store the generated code in a file named after the first human message
+    with open(filename_0, "w") as file:
+        file.write(str(generated_code_0_list))
+
+    # Print a success message
+    print(f"Generated code saved to {filename_0}")
+
+    filename_1 = os.path.join(dir, "p" + str(file_index) + ".1_" + "" .join(msg_1[0:100].split()) + ".py")
+    # Store the generated code in a file named after the first human message
+    with open(filename_1, "w") as file:
+        file.write(str(generated_code_1_list))
+
+    # Print a success message
+    print(f"Generated code saved to {filename_1}")
+    file_index +=1
---
+++ ChatGPT_api_prompts/p13_auto_revere_linkedlist_strategy.py
@@ -0,0 +1,160 @@
+import ast
+import os
+import pathlib
+import re
+import random
+import statistics
+import timeit
+import csv
+from collections import defaultdict
+
+#**************************************************************
+# Problem p13: Reversing a linked list 
+# Source: ChatGPT API
+#**************************************************************
+
+problem = "p13_auto_revere_linkedlist_strategy"
+output = "p13_auto_execution_times.csv"
+
+dir = os.path.dirname(__file__)
+new_dir_name = 'AutomatePromptingCodeGeneration/data/'
+
+# Directory path containing the JSON files
+directory_path = pathlib.Path(dir, new_dir_name)
+prefix = "p12."
+suffix = ".py"
+
+function_index = 0
+p_result_list = []
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.data = val
+        self.value = val
+        self.next = None
+
+# Generate a linked list with 100 random numbers with a range of 1 to 200
+head = Node(random.randint(1, 200))
+current = head
+for i in range(100):
+    current.next = Node(random.randint(1, 200))
+    current = current.next
+
+# Loop through all the files in the directory
+for filename in os.listdir(directory_path):
+    # Check if the file has a .json extension
+    if filename.startswith(prefix) and filename.endswith(suffix):
+        # Construct the full file path
+        file_path = os.path.join(directory_path, filename)
+        
+        print(file_path)
+
+        # Open the file in read mode
+        with open(file_path, 'r') as file:
+            # Read the file content
+            file_content = file.read()
+            data_dict = file_content.split("{\'code\':")
+
+            sample_index = 0
+            print(f"function_index: {function_index}")
+            for data in data_dict:
+                try:
+                    code_start_index = data.replace("```python","```Python").find("```Python")
+                    print(f"\t sample_index: {sample_index}")
+                    if code_start_index > 0: 
+                        code_end_index = data.find("```", code_start_index + len("```Python"))
+                        code_string = data[code_start_index + len("```Python"):code_end_index]
+                        # Cleaning up the code string and replacing "\n" with actual newline character
+                        code_string = code_string.strip().replace("\\n", "\n")
+                        # Removing escape sequences
+                        code_string = code_string.encode().decode('unicode_escape')
+                        # Evaluating the code string and capturing the AST
+                        code_ast = ast.parse(code_string, mode='exec')
+                        # Extracting the source code from the AST
+                        code_segment = ast.unparse(code_ast)
+                        # Accessing the extracted code
+                        # print(code_segment)
+
+                        # Executing the code segment
+                        exec(code_segment)
+
+                        # # Example data
+                        # # Calling the function with example data
+                        # result = funcImp1(head)
+                        # # Printing the result
+                        # print("\t\tResult:", result.val)
+
+                        #**************************************************************
+                        sizes = [1000, 10000, 1000000]
+                        # sizes = [1000, 2000, 5000]
+                        # sizes = [100, 200, 500]
+                        versions = 100
+
+                        for size in sizes:
+                            time_list = []
+                            print(f"\t\tTesting for list size {size}")
+                            for i in range(versions):
+                                # Generate a linked list with 1000000 random numbers with a range of 0 to size
+                                head = Node(random.randint(0, 1000000))
+                                current = head
+                                for i in range(size):
+                                    current.next = Node(random.randint(0, 1000000))
+                                    current = current.next
+
+                                time_res = timeit.timeit(lambda: funcImp1(head), number=100)
+                                time_list.append(time_res)
+
+                            min_time = min(time_list)
+                            avg_time = statistics.mean(time_list)
+                            max_time = max(time_list)
+
+                            result = {
+                                'function_index': function_index,
+                                'sample_index': sample_index,
+                                'size': size,
+                                'min_time': min_time,
+                                'avg_time': avg_time,
+                                'max_time': max_time
+                            }
+                            p_result_list.append(result)
+                        # print(\t\t p_result_list)
+                        sample_index+=1
+                    else:
+                        print(f"\t\t function_index: {function_index} , code_start_index: {code_start_index}")
+                        sample_index+=1
+                except Exception as e:
+                    print (f"\t\t function_index: {function_index} , code_start_index: {code_start_index}, Error: {str(e)} ")
+                    sample_index +=1
+                    continue
+        function_index +=1
+
+result = defaultdict(lambda: defaultdict(list))
+
+for row in p_result_list:
+    file_index = row['function_index']
+    size = row['size']
+
+    result[(file_index, size)]['min_time'].append(row['min_time'])
+    result[(file_index, size)]['avg_time'].append(row['avg_time'])
+    result[(file_index, size)]['max_time'].append(row['max_time'])
+
+dir = os.path.dirname(__file__)
+
+with open(os.path.join(dir,output), mode='a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Size', 'Function', 'Min', 'Average', 'Max'])
+    for (function_index, size), times in result.items():
+        print(f"Size: {size}")
+        print(f"function Index: {function_index}")
+        min_time = min(times['min_time'])
+        avg_time = sum(times['avg_time'])/len(times['avg_time'])
+        max_time = max(times['max_time'])
+
+        print(f"Summary *******************************************")
+        print(f"Min of min_time: {min_time}")
+        print(f"Avg of avg_time: {avg_time}")
+        print(f"Max of max_time: {max_time}")
+        print()
+
+        writer.writerow([size, str(function_index) + "_" + problem , min_time, avg_time, max_time])
